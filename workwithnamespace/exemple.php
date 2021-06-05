3602c409e861b50b90b7cb6d91c4f052f400790f
chokrihamza <chokrihamza1994@gmail.com>
2021-06-05 16:09:17 +0200

    work with php namaspace1

--- workwithnamespace/exemple.php
+++ workwithnamespace/exemple.php
@@ -1,6 +1,12 @@
 <?php 
 
-require('class/DateTime.php');
+spl_autoload_register(function($class){
+  
+
+  require 'class/'.str_replace('App\Date\\','',$class).'.php';
+ 
+
+});
 
 $date=new App\Date\DateTime();
 $date->showTime();
